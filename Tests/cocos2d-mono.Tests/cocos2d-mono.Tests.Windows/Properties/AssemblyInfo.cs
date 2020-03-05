b5f4e52ca300577e5aa6e3a1231de9ebbd36b28d
brandmooffin <brandmooffin@outlook.com>
2020-03-05 01:27:36 -0500

    Cleaning up windows test bed

--- Tests/cocos2d-mono.Tests/cocos2d-mono.Tests.Windows/Properties/AssemblyInfo.cs
+++ Tests/cocos2d-mono.Tests/cocos2d-mono.Tests.Windows/Properties/AssemblyInfo.cs
@@ -4,6 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("Cocos2D-Mono Tests for Windows")]
+[assembly: AssemblyDescription("Cocos2D-Mono Tests for Windows (DX)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Broken Walls Studios")]
 [assembly: AssemblyProduct("cocos2d-mono.Tests.Android")]
