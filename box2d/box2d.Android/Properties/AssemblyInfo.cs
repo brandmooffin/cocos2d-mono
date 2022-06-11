3200c1f6a1d982d84345c70aa4b80825c721ee0b
Brandon Roberts <brandmooffin@outlook.com>
2022-06-11 12:24:46 -0400

    Prep for Release 2.3.9 (#120)

--- box2d/box2d.Android/Properties/AssemblyInfo.cs
+++ box2d/box2d.Android/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Broken Walls Studios")]
 [assembly: AssemblyProduct("box2d")]
-[assembly: AssemblyCopyright("Copyright © 2020 Cocos2D-Mono Team; Broken Walls Studios, LLC.")]
+[assembly: AssemblyCopyright("Copyright © 2022 Cocos2D-Mono Team; Broken Walls Studios, LLC.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
