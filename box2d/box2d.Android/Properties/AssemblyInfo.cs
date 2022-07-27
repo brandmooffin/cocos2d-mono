f08820d4a42bb86597b68f4640362c0dce33b7e9
Brandon Roberts <brandmooffin@outlook.com>
2022-07-27 16:54:33 -0400

    Prep for release 2.3.10

--- box2d/box2d.Android/Properties/AssemblyInfo.cs
+++ box2d/box2d.Android/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("box2d for Android")]
-[assembly: AssemblyDescription("Box2D for Android (4.4+)")]
+[assembly: AssemblyDescription("Box2D for Android (9.0+)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Broken Walls Studios")]
 [assembly: AssemblyProduct("box2d")]
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
