2d282f76b44b706005f186fffbaf91512cce2015
Brandon Roberts <brandmooffin@outlook.com>
2023-06-28 19:55:21 -0400

    Initial sample setup

---
+++ Samples/AppGame/AppGame.Shared/Scenes/IntroScene.cs
@@ -0,0 +1,25 @@
+﻿using Cocos2D;
+
+namespace AppGame.Shared.Scenes
+{
+    public class IntroScene : CCScene
+    {
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            var size = CCDirector.SharedDirector.WinSize;
+
+            var highScoreTitleLabel = new CCLabelTTF("Hello World!", "MarkerFelt", 13);
+            highScoreTitleLabel.Color = CCColor3B.White;
+            highScoreTitleLabel.Position = new CCPoint(size.Width - 100, size.Height - 30);
+            AddChild(highScoreTitleLabel);
+
+            ScheduleUpdate();
+        }
+
+        public override void Update(float gameTime)
+        {
+          
+        }
+    }
+}
