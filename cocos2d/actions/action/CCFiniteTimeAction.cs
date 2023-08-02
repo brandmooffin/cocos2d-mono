4bf79ff22239d32f6c644aaab375df0ab0ee371a
Brandon Roberts <brandmooffin@outlook.com>
2023-08-02 17:32:31 -0400

    add CCActionState

--- cocos2d/actions/action/CCFiniteTimeAction.cs
+++ cocos2d/actions/action/CCFiniteTimeAction.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Cocos2D
 {
     public class CCFiniteTimeAction : CCAction
@@ -30,5 +32,51 @@ namespace Cocos2D
         {
             return null;
         }
+    }
+
+    public class CCFiniteTimeActionState : CCActionState
+    {
+        bool firstTick;
+
+        #region Properties
+
+        public virtual float Duration { get; set; }
+        public float Elapsed { get; private set; }
+
+        public override bool IsDone
+        {
+            get { return Elapsed >= Duration; }
+        }
+
+        #endregion Properties
+
+
+        public CCFiniteTimeActionState(CCFiniteTimeAction action, CCNode target)
+            : base(action, target)
+        {
+            Duration = action.Duration;
+            Elapsed = 0.0f;
+            firstTick = true;
+        }
+
+        protected internal override void Step(float dt)
+        {
+            if (firstTick)
+            {
+                firstTick = false;
+                Elapsed = 0f;
+            }
+            else
+            {
+                Elapsed += dt;
+            }
+
+            Update(Math.Max(0f,
+                Math.Min(1, Elapsed / Math.Max(Duration, float.Epsilon)
+                )
+            )
+            );
+        }
+
     }
 }
