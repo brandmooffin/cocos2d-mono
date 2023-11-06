8471255f6fe9cb30541f1f2c031bea0e4f341c8f
Brandon Roberts <brandmooffin@outlook.com>
2023-11-05 21:32:31 -0500

    removed unused references

--- cocos2d/misc_nodes/CCDrawNode.cs
+++ cocos2d/misc_nodes/CCDrawNode.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Drawing.Drawing2D;
 using System.Linq;
-using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
