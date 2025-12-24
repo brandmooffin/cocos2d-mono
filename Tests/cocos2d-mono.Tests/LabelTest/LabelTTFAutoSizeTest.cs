226d639dde6576e1f5260a48d62b1caefae552a2
brandmooffin <brandmooffin@outlook.com>
2025-12-24 10:54:32 -0500

    add more custom fonts for testing

--- Tests/cocos2d-mono.Tests/LabelTest/LabelTTFAutoSizeTest.cs
+++ Tests/cocos2d-mono.Tests/LabelTest/LabelTTFAutoSizeTest.cs
@@ -9,15 +9,9 @@ namespace tests
     {
         private CCTextAlignment m_eHorizAlign;
         private CCVerticalTextAlignment m_eVertAlign;
-        private CCLabelTTF m_plabel;
-        private CCLabelTTF m_label2;
-
-        private SpriteFont customFont;
 
         public LabelTTFAutoSizeTest()
         {
-            customFont = CCContentManager.SharedContentManager.Load<SpriteFont>("fonts/SFFedora-32");
-
             var blockSize = new CCSize(400, 160);
             CCSize s = CCDirector.SharedDirector.WinSize;
 
@@ -27,7 +21,6 @@ namespace tests
 
             AddChild(colorLayer);
 
-            m_plabel = null;
             updateAlignment();
         }
 
@@ -36,30 +29,26 @@ namespace tests
             var blockSize = new CCSize(400, 160);
             CCSize s = CCDirector.SharedDirector.WinSize;
 
-            m_label2 = new CCLabelTTF("AutoSize Font", "SFFedoraTitles", 32, new CCSize(400, 64), CCTextAlignment.Left, CCVerticalTextAlignment.Center);
+            var m_label2 = new CCLabelTTF("AutoSize Font #1", "SFFedoraTitles", 32);
             m_label2.AnchorPoint = new CCPoint(0, 0);
             m_label2.Position = new CCPoint((s.Width - blockSize.Width) / 2, (s.Height - blockSize.Height +150) / 2);
 
             AddChild(m_label2);
 
-            m_plabel = new CCLabelTTF("AutoSize Custom Font", "SFFedora", 32);
+            var m_plabel = new CCLabelTTF("AutoSize Custom Font 2", "SFFedora", 32);
             m_plabel.AnchorPoint = new CCPoint(0, 0);
-            //m_plabel.VerticalAlignment = CCVerticalTextAlignment.Center;
             m_plabel.Position = new CCPoint((s.Width - blockSize.Width) / 2, (s.Height - blockSize.Height) / 2);
 
 
             AddChild(m_plabel);
-        }
 
-        public override void Draw()
-        {
-            base.Draw();
+            var m_label3 = new CCLabelTTF("AutoSize Font", "Abduction", 26);
+            m_label3.Position = new CCPoint((s.Width - blockSize.Width) / 2, (s.Height - blockSize.Height) / 4);
+            AddChild(m_label3);
 
-            CCDrawManager.spriteBatch.Begin();
-
-            CCDrawManager.spriteBatch.DrawString(customFont, "AutoSize Custom Font", new CCPoint(m_plabel.PositionX, m_plabel.PositionY - 200), Microsoft.Xna.Framework.Color.White);
-
-            CCDrawManager.spriteBatch.End();
+            var m_label4 = new CCLabelTTF("AutoSize Font 4", "arial", 64);
+            m_label4.Position = new CCPoint((s.Width - blockSize.Width) / 2, (s.Height - blockSize.Height) / 6);
+            AddChild(m_label4);
         }
 
         public override string title()
